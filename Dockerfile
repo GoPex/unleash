fbbf78de4add9160444422aaf4b184a6996c831c
Albin Gilles <gilles.albin@gmail.com>
2016-02-13 07:58:17 +0100

    Dev ongoing

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
 # From official golang alpine image
 FROM gopex/ubuntu_golang:1.6rc1
-MAINTAINER Albin Gilles "albin.gilles@gmail.com"
-ENV REFRESHED_AT 2016-02-04
+MAINTAINER Albin Gilles "gilles.albin@gmail.com"
+ENV REFRESHED_AT 2016-02-12.1
 
 # Port exposed by this application
 EXPOSE 3000
 
 # Prepare directory holding our application
-RUN mkdir $GOPATH/src/github.com/GoPex/unleash
+RUN mkdir -p $GOPATH/src/bitbucket.org/gopex/unleash
 
-# Configure the GOPATH to our application
-WORKDIR $GOPATH/src/github.com/GoPex/unleash
+# Set the working directory
+WORKDIR $GOPATH/src/bitbucket.org/gopex/unleash
+
+# Copy our application into the container
 COPY . .
+
+# Get dependencies
+RUN go get
--- server.go
+++ server.go
@@ -2,16 +2,50 @@ package main
 
 import (
     "fmt"
-    "html"
-    "log"
-    "net/http"
+    "time"
+
+    "github.com/gin-gonic/gin"
 )
 
-func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+type Login struct {
+    User     string `form:"user" json:"user" binding:"required"`
+    Password string `form:"password" json:"password" binding:"required"`
+}
+
+func process() {
+    for {
+        fmt.Println("processing")
+        time.Sleep(time.Millisecond * 1000)
+    }
+}
+
+func ping(c *gin.Context) {
+    go process()
+
+    c.JSON(200, gin.H{
+        "message": "pong",
     })
 
-    log.Fatal(http.ListenAndServe(":3000", nil))
+}
 
+func loginJSON(c *gin.Context) {
+    var json Login
+    if c.BindJSON(&json) == nil {
+        fmt.Println(json.User)
+        fmt.Println(json.Password)
+    }
+    fmt.Println(c)
+}
+
+func main() {
+
+    // Create a default gin stack
+    r := gin.Default()
+
+    // Routes
+    r.GET("/ping", ping)
+    r.POST("/loginJSON", loginJSON)
+
+    // Unleash!
+    r.Run() // listen and serve on port defined by environment variable PORT
 }
